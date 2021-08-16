0f340f56df513217627a2bcd1c9502887bf1b369
Eugene Yavgel <eugene.yavgel@gmail.com>
2021-08-17 00:44:24 +0300

    added header component

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,15 +1,18 @@
 <template>
     <Navbar />
+    <Header />
 
     <div class="bg-gray-100 h-screen"></div>
 </template>
 
 <script>
     import Navbar from '../Components/Navbar'
+    import Header from '../Components/Header'
 
     export default {
         components: {
-            Navbar
+            Navbar,
+            Header
         },
         props: {}
     }
