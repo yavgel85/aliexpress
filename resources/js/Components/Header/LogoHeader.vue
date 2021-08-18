eb370ff9731c69061b4cb9a69796252b99c6c869
Eugene Yavgel <eugene.yavgel@gmail.com>
2021-08-18 16:33:59 +0300

    refacroring header component

--- resources/js/Components/Header/LogoHeader.vue
+++ resources/js/Components/Header/LogoHeader.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="w-1/4 mr-3">
-        <img src="/images/aliexpress-logo.png" alt="">
+        <LogoImage />
         <span class="text-xs text-gray-500">Smart Shopping, Better Living!</span>
     </div>
 </template>
+
+<script>
+    import LogoImage from '../Images/LogoImage'
+
+    export default {
+        components: {
+            LogoImage
+        }
+    }
+</script>
